f30159ba4175427cf320c58dbc46b3e999d558ac
dcdmmcomeon <dcdmmcomeon@gmail.com>
2024-01-12 20:33:42 +0800

    树周末搞定.no time

--- B_树/BinaryTree.py
+++ B_树/BinaryTree.py
@@ -1,5 +1,6 @@
 from Tree import Tree
 
+
 class BinaryTree(Tree):
     """Abstract base class representing a binary tree structure."""
 
@@ -38,6 +39,89 @@ class BinaryTree(Tree):
         if self.right(p) is not None:
             yield self.right(p)
 
-if __name__ == '__main__':
 
-    print(234324)
+class LinkedList(BinaryTree):
+    """Linked representation of a binary tree structure."""
+
+    class _Node:  # Lightweight, nonpublic class for storing a node.
+        __slots__ = "_element", "_parent", "_left", "_right"
+
+        def __init__(self, element, parent=None, left=None, right=None):
+            self._element = element
+            self._parent = parent
+            self._left = left
+            self._right = right
+
+    class Position(BinaryTree.Position):
+        """An abstraction representing the location of a single element."""
+
+        def __init__(self, container, node):
+            """Constructor should not be invoked by user."""
+            self._container = container
+            self._node = node
+
+        def element(self):
+            """Return the element stored at this Position."""
+            return self._node._element
+
+        def __eq__(self, other):
+            """Return True if other is a Position representing the same location."""
+            return type(other) is type(self) and other._node is self._node
+
+    def _validate(self, p):
+        """Return associated node, if position is valid."""
+        if not isinstance(p, self.Position):
+            raise TypeError('p must be proper Positon type')
+        if p._container is not self:
+            raise ValueError("p does not belong to the container")
+        if p._node._parent is p._node:
+            raise ValueError('p is no longer valid')
+        return p._node
+
+    def _make_position(self, node):
+        """Return Position instance for given node (or None if no node)."""
+        return self.Position(self, node) if node is not None else None
+
+    # -------------------------- binary tree constructor --------------------------
+    def __init__(self):
+        """Create an initially empty binary tree."""
+        self._root = None
+        self._size = 0
+
+    # -------------------------- public accessors --------------------------
+    def __len__(self):
+        """Return the total number of elements in the tree."""
+        return self._size
+
+    def root(self):
+        """Return the root Position of the tree (or None if tree is empty)."""
+        return self._make_position(self._root)
+
+    def parent(self, p):
+        """Return the Position of p s parent (or None if p is root)."""
+        node = self._validate(p)
+        return self._make_position(node._parent)
+
+    def left(self, p):
+        """Return the Position of p s left child (or None if no left child)."""
+        node = self._validate(p)
+        return self._make_position(node._left)
+
+    def right(self, p):
+        """Return the Position of p s right child (or None if no right child)."""
+        node = self._validate(p)
+        return self._make_position(node._right)
+
+    def num_children(self, p):
+        """Return the number of children of Position p."""
+        node = self._validate(p)
+        count = 0
+        if node._left is not None:  # left child exists
+            count += 1
+        if node._right is not None:  # risht child exists
+            count += 1
+        return count
+
+
+if __name__ == '__main__':
+    pass
