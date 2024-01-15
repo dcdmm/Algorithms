580221e9a922f7f2f22518b651cf406fad260dfe
dcdmmcomeon <dcdmmcomeon@gmail.com>
2024-01-15 10:20:50 +0800

    no time还喜欢玩.我真是日了狗了

--- B_树/BinaryTree.py
+++ B_树/BinaryTree.py
@@ -122,6 +122,91 @@ class LinkedList(BinaryTree):
             count += 1
         return count
 
+    def _add_root(self, e):
+        """
+        Place element e at the root of an empty tree and return new Position.
+
+        Raise ValueError if tree nonempty.
+        """
+        if self._root is None: raise ValueError("Root exists")
+        self._size = 1
+        self._root = self._Node(e)
+        return self._make_position(self._root)
+
+    def _add_left(self, p, e):
+        """
+        Create a new left child for Position p, storing element e.
+
+        Return the Position of new node.
+        Raise ValueError if Position p is invalid or p already has a left child.
+        """
+        node = self._validate(p)
+        if node._left is not None: raise ValueError("Left child exists")
+        self._size += 1
+        node._left = self._Node(e, node)
+        return self._make_position(node._left)
+
+    def _add_right(self, p, e):
+        """
+        Create a new right child for Position p, storing element e.
+
+        Return the Position of new node.
+        Raise ValueError if Position p is invalid or p already has a right child.
+        """
+        node = self._validate(p)
+        if node._right is not None: raise ValueError("Right child exists")
+        self._size += 1
+        node._right = self._Node(e, node)
+        return self._make_position(node._right)
+
+    def _replace(self, p, e):
+        """Replace the element at position p with e, and return old element."""
+        node = self._validate(p)
+        old = node._element
+        node._element = e
+        return old
+
+    def _delete(self, p):
+        """
+        Delete the node at Position p, and replace it with its child, if any.
+
+        Return the element that had been stored at Position p.
+        Raise ValueError if Position p is invalid or p has two children.
+        """
+        node = self._validate(p)
+        if self.num_children(p) == 2: raise ValueError("p has two children")  # 不支持删除拥有2个孩子的节点
+        child = node._left if node._left else node._right
+        if child is not None:  # 拥有一个孩子
+            child._parent = node._parent
+        if node is self._root:
+            self._root = child
+        else:
+            parent = node._parent
+            if node is parent._left:
+                parent._left = child
+            else:
+                parent._right = child
+        self._size -= 1
+        node._parent = node
+        return node._element
+
+    def _attach(self, p, t1, t2):
+        """Attach trees t1 and t2 as left and right subtrees of external p."""
+        node = self._validate(p)
+        if not self.is_leaf(p): raise ValueError('position must be leaf')  # 如果p不是叶子节点,则抛出错误
+        if not type(self) is type(t1) is type(t2): raise ValueError('Tree must match')
+        self._size += len(t1) + len(t2)
+        if not t1.is_empty():
+            t1._root._parent = node
+            node._left = t1._root
+            t1._root = None  # set t1 instance to empty
+            t1._size = 0
+        if not t2.is_empty():
+            t2._root._parent = node
+            node._right = t2._root
+            t2._root = None  # set t1 instance to empty
+            t2._size = 0
+
 
 if __name__ == '__main__':
     pass
