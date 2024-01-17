7faec2f73ef9caf34592bd2705d62a7fab2ec61d
dcdmmcomeon <dcdmmcomeon@gmail.com>
2024-01-17 12:11:02 +0800

    树理论学习完毕

--- B_树/TreeMap.py
+++ B_树/TreeMap.py
@@ -26,7 +26,9 @@ class MapBase(MutableMapping):
 
 
 class TreeMap(LinkedBinaryTree, MapBase):
-    """Sorted map implementation using a binary search tree."""
+    """
+    Sorted map implementation using a binary search tree.
+    """
 
     # ---------------------------- override Position class ----------------------------
     class Position(LinkedBinaryTree.Position):
@@ -66,15 +68,15 @@ class TreeMap(LinkedBinaryTree, MapBase):
             walk = self.right(walk)
         return walk
 
-    def first(self):
+    def first(self):  # 时间复杂度:O(h),其中h为树的高度
         """返回一个包含最小键的节点,如果树为空,则返回None"""
         return self._subtree_first_positon(self.root()) if len(self) > 0 else None
 
-    def last(self):
+    def last(self):  # 时间复杂度:O(h),其中h为树的高度
         """返回一个包含最大键的节点,如果树为空,则返回None"""
         return self._subtree_last_positon(self.root()) if len(self) > 0 else None
 
-    def before(self, p):
+    def before(self, p):  # 时间复杂度:O(h),其中h为树的高度
         """
         返回比节点p的键小的所有节点中键最大的节点(即中序遍历中在p之前最后一个被访问的节点)
 
@@ -94,7 +96,7 @@ class TreeMap(LinkedBinaryTree, MapBase):
                 above = self.parent(walk)
             return above
 
-    def after(self, p):
+    def after(self, p):  # 时间复杂度:O(h),其中h为树的高度
         """
         返回比节点p的键大的所有节点中键最小的节点(即中序遍历中在p之后第一个被访问的节点)
         """
@@ -109,10 +111,75 @@ class TreeMap(LinkedBinaryTree, MapBase):
                 above = self.parent(walk)
             return above
 
-    def find_min(self):
+    def find_min(self):  # 时间复杂度:O(h),其中h为树的高度
         """Return (key,value) pair with minimum key (or None if empty)."""
         if self.is_empty():
             return None
         else:
             p = self.first()
             return (p.key(), p.value())
+
+    def find_max(self):  # 时间复杂度:O(h),其中h为树的高度
+        """Return (key,value) pair with maximum key (or None if empty)."""
+        if self.is_empty():
+            return None
+        else:
+            p = self.last()
+            return (p.key(), p.value())
+
+    def __getitem__(self, k):  # 时间复杂度:O(h),其中h为树的高度
+        """Return value associated with key k (raise KeyError if not found)."""
+        if self.is_empty():
+            raise KeyError("Key Error: " + repr(k))
+        else:
+            p = self._subtree_search(self.root(), k)
+            if k != p.key():  # 没有找到
+                raise KeyError("Key Error: " + repr(k))
+            return p.value
+
+    def __setitem__(self, k, v):  # 时间复杂度:O(h),其中h为树的高度
+        """Assign value v to key k, overwriting existing value if present."""
+        if self.is_empty():
+            self._add_root(self._Item(k, v))
+        else:
+            p = self._subtree_search(self.root(), k)
+            if p.key() == k:
+                p.element()._value = v  # replace existing item’s value
+                return
+            else:
+                # 新的节点可以插到树的下一层
+                item = self._Item(k, v)
+                if p.key() < k:
+                    self._add_right(p, item)  # inherited from LinkedBinaryTree
+                else:
+                    self._add_left(p, item)
+
+    def delete(self, p):  # 时间复杂度:O(h),其中h为树的高度
+        """Remove the item at given Position."""
+        self._validate(p)  # inherited from LinkedBinaryTree
+        if self.left(p) and self.right(p):  # p has two children
+            replacement = self._subtree_last_positon(self.left(p))  # 比节点p的键小的所有节点中键最大的节点r(肯定没有2个子节点,不然其右子节点的键肯定比p大)
+            self._replace(p, replacement.element())  # 使用t节点的值替换p节点原有的值
+            p = replacement  # 删除r节点(r节点最多子只有1个子节点)
+        # now p has at most one child
+        self._delete(p)  # inherited from LinkedBinaryTree
+
+    def __delitem__(self, k):  # 时间复杂度:O(h),其中h为树的高度
+        """Remove item associated with key k (raise KeyError if not found)."""
+        if not self.is_empty():
+            p = self._subtree_search(self.root(), k)
+            if k == p.key():
+                self.delete(p)
+                return
+        raise KeyError("Key Error: " + repr(k))
+
+    def __iter__(self):  # 时间复杂度:O(n)
+        """Generate an iteration of all keys in the map in order."""
+        p = self.first()
+        while p is not None:
+            yield p.key()
+            p = self.after(p)
+
+
+if __name__ == '__main__':
+    pass
